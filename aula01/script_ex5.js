46642ce96de1fc97ff3d557c3320f17ff43b6fd5
Gustavo Lopes <gustavo.souza0911@gmail.com>
2024-08-06 14:26:54 -0300

    Coloca os exercicios das Olimpiadas no repositorio

---
+++ aula01/script_ex5.js
@@ -0,0 +1,14 @@
+let DistanciaA,SegundosDesdeLancar1;
+let DistanciaB,SegundosDesdeLancar2;
+
+
+    SegundosDesdeLancar1 = 7;
+    DistanciaA = (-SegundosDesdeLancar1 * -SegundosDesdeLancar1) + (6 * SegundosDesdeLancar1) + 10;
+
+
+    SegundosDesdeLancar2 = 9;
+    DistanciaB = (-0.5 *(SegundosDesdeLancar2 * SegundosDesdeLancar2)) + 4 * SegundosDesdeLancar2 + 12;
+
+    
+console.log("O valor da distância do A é de: " + DistanciaA)
+console.log("O valor da distância do B é de: " + DistanciaB)
