56b044aa82b171984cfac77b27e138059ca58e7e
Gustavo Lopes <gustavo.souza0911@gmail.com>
2024-08-13 14:36:32 -0300

    Resolve exercicios 5-6 dados pelos professores.

---
+++ aula01/Exercicio 5/script_ex5.js
@@ -0,0 +1,26 @@
+// Ian Raçano Gonzalez 
+// Gustavo Souza Lopes
+// 13 / 08 / 2024
+
+let DistanciaA,SegundosDesdeLancar1;
+let DistanciaB,SegundosDesdeLancar2;
+
+
+    SegundosDesdeLancar1 = 3;
+    DistanciaA = (-SegundosDesdeLancar1 * SegundosDesdeLancar1) + (6 * SegundosDesdeLancar1) + 10;
+
+
+    SegundosDesdeLancar2 = 4;
+    DistanciaB = (-0.5 *(SegundosDesdeLancar2 * SegundosDesdeLancar2)) + 4 * SegundosDesdeLancar2 + 12;
+
+    
+
+console.log("O valor da distância do A é de: " + DistanciaA)
+console.log("O valor da distância do B é de: " + DistanciaB)
+
+console.log("Portanto a distancia do atleta B por ser: " + DistanciaB + " é maior que do Atleta A: " + DistanciaA)
+
+// 1. Atleta B alcança a maior distancia.
+
+// 2. (Atleta B = 20 metros)
+//  (Atleta A = 19)
