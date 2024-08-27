c5ff6da2e9abf7a58c4dd9f855703c493e1f4384
Gustavo Lopes <gustavo.souza0911@gmail.com>
2024-08-27 14:33:37 -0300

    Sobe exercicios atualizados

--- aula01/Exercicio 5 e 6/Exercicio 5/script_ex5.js
+++ aula01/Exercicio 5 e 6/Exercicio 5/script_ex5.js
@@ -6,19 +6,22 @@ let DistanciaA,SegundosDesdeLancar1;
 let DistanciaB,SegundosDesdeLancar2;
 
 
-    SegundosDesdeLancar1 = 3;
+ SegundosDesdeLancar1 = prompt("Digite os segundos desde o lançamento 1:  "); 
     DistanciaA = (-SegundosDesdeLancar1 * SegundosDesdeLancar1) + (6 * SegundosDesdeLancar1) + 10;
 
 
-    SegundosDesdeLancar2 = 4;
+    
+
+
+     SegundosDesdeLancar2 =  prompt("Digite os segundos desde o lançamento 2:  ");
     DistanciaB = (-0.5 *(SegundosDesdeLancar2 * SegundosDesdeLancar2)) + 4 * SegundosDesdeLancar2 + 12;
 
     
 
-console.log("O valor da distância do A é de: " + DistanciaA)
-console.log("O valor da distância do B é de: " + DistanciaB)
+alert("O valor da distância do A é de: " + DistanciaA);
+alert("O valor da distância do B é de: " + DistanciaB)
 
-console.log("Portanto a distancia do atleta B por ser: " + DistanciaB + " é maior que do Atleta A: " + DistanciaA)
+alert("Portanto a distancia do atleta B por ser: " + DistanciaB + " é maior que do Atleta A: " + DistanciaA)
 
 // 1. Atleta B alcança a maior distancia.
 
